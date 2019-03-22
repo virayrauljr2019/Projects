5b254f87a3769e066030fbeaf8a27c8a176f65c9
virayrauljr2019 <42677973+virayrauljr2019@users.noreply.github.com>
2019-03-21 23:17:36 -0700

    Add files via upload

---
+++ Bank/CustomerAccount.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+public class CustomerAccount extends ATMmachine {
+
+    public CustomerAccount(String Account) {
+        Account1=Account;
+        String Account[];
+
+
+        Account = new String[8];
+    }
+}
