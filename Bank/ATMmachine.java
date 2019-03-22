67e499036739b8479b2959af249dc3a17e4008a9
virayrauljr2019 <42677973+virayrauljr2019@users.noreply.github.com>
2019-03-21 23:17:52 -0700

    Add files via upload

---
+++ Bank/ATMmachine.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class ATMmachine extends Main {
+    public static Scanner scan;
+    String Account1;
+    String Account[];
+    public static void credit() {
+        scan = new
+
+                Scanner(System.in);
+
+        String test = scan.nextLine();
+account.add(scanner);
+        switch (test) {
+
+
+        }
+    }
+}
+
+
